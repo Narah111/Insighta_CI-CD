6b9e9abcba8548172b20466ae6078f1d5b83d29c
Nasir <rahmanzadan@gmail.com>
2025-09-10 18:38:39 +0200

    uppdated the message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello, Insighta!</h1>'
+    return '<h1>Hello, Insighta! \n This is an uppdate after deploying in Google Cloud with docker container :) </h1>'
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
