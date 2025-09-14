8e689e527fa7cdea9f5e4367a2947b939f2a4217
Nasir <rahmanzadan@gmail.com>
2025-09-14 16:12:05 +0200

    changed to use app context

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,12 +2,13 @@ from app import app
 
 def test_index():
     #Given
-    app.testing = True                         #<---  Enable testing mode
-    with app.test_client() as client:
+    app.testing = True 
+    with app.app_context():                      #<--- Use app context
+        with app.test_client() as client:
 
-        #When
-        response = client.get ('/')
+            #When
+            response = client.get ('/')
 
-        #Then
-        assert response.status_code == 200
-        assert b'Hello, Insighta!' in response.data
+            #Then
+            assert response.status_code == 200
+            assert b'Hello, Insighta!' in response.data
